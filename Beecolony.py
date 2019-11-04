3208bf194a94946cbac50d275b55c47670944a0b
ayush737 <31165568+ayush737@users.noreply.github.com>
2019-11-04 17:07:56 +0530

    Update Beecolony.py
    Connected graph drawer

--- Beecolony.py
+++ Beecolony.py
@@ -150,3 +150,47 @@ def sandbox():
 
 
 main()
+
+#connected graph maker using nodes and edges
+
+#import networkx as nx
+
+#G=nx.Graph()
+
+#print(G.nodes())
+#print(G.edges())
+
+#print(type(G.nodes()))
+#print(type(G.edges()))
+
+#adding nodes
+#G=nx.Graph()
+
+# adding just one node:
+#G.add_node("a")
+# a list of nodes:
+#G.add_nodes_from(["b","c"])
+
+#print("Nodes of graph: ")
+#print(G.nodes())
+#print("Edges of graph: ")
+#print(G.edges())
+# edges
+#G=nx.Graph()
+#G.add_node("a")
+#G.add_nodes_from(["b","c"])
+
+#G.add_edge(1,2)
+#edge = ("d", "e")
+#G.add_edge(*edge)
+#edge = ("a", "b")
+#G.add_edge(*edge)
+
+#print("Nodes of graph: ")
+#print(G.nodes())
+#print("Edges of graph: ")
+#print(G.edges())
+#G.add_edges_from([("a","c"),("c","d"), ("a",1), (1,"d"), ("a",2)])
+#nx.draw(G)
+#plt.savefig("simple_path.png") # save as png
+#plt.show() # display
